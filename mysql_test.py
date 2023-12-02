2c6c3c92a9716d8d7590f0920411fd9dfbe0a1f2
jsan8x <104663293+jsan8x@users.noreply.github.com>
2023-12-02 14:30:12 -0800

    Add files via upload

---
+++ mysql_test.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "pysports_user",
+    "password": "P@ssw0rd",
+    "host": "3306",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
